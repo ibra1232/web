dd50fe09a3d53583bab606c056b18889b629e34f
ibra1232 <Ibrahim.belgharek@sesame.com.tn>
2024-08-27 16:05:41 -0700

    hahahahhaah

--- web.js
+++ web.js
@@ -2,15 +2,25 @@ var Plus = document.getElementsByClassName('fa-plus-circle');
 var Minus = document.getElementsByClassName('fa-minus-circle');
 var quantities = document.getElementsByClassName('quantity');
 var favs =document.getElementsByClassName('fa-heart');
+var pb =document.getElementsByClassName('fa-trash-alt');
+var card=document.getElementsByClassName('card-body');
 console.log(favs)
 
 
 for (let i = 0; i < favs.length; i++) {
     favs[i].addEventListener('click', function() {
-        if (favs[i].style.color=="black") {
-            favs[i].style.color="red";
-        }})
-    }
+        if (this.style.color !== "red") {
+            this.style.color = "red";
+        } else {
+            this.style.color = "black";
+        }
+    });
+}
+for (let i = 0; i < pb.length; i++) {
+    pb[i].addEventListener('click', function() {
+        card[i].remove()
+    });
+}
   
 for (let i = 0; i < Plus.length; i++) {
     Plus[i].addEventListener('click', function() {
