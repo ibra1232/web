b51b4a7e14d08f60cf770a5d22c3cea146bf3b10
ibra1232 <Ibrahim.belgharek@sesame.com.tn>
2024-08-26 18:46:40 -0700

    hahaha

--- web.js
+++ web.js
@@ -1,17 +1,57 @@
-var fa-plus-circle =document.getElementsByClassName('fa-plus-circle');
-var fa-minus-circle =document.getElementsByClassName('fa-minus-circle');
-var quantity =document.getElementsByClassName('quantity');
-var cpt = parseInt(quantity.innerText);
-fa-plus-circle.addEventListener('click',function(){
-    cpt = cpt+1;
-    quantity.innerHTML = cpt;
-});
-fa-minus-circle.addEventListener('click',function(){
-    if(cpt>0) {
-    cpt = cpt-1;
-    quantity.innerHTML = cpt;
+var Plus = document.getElementsByClassName('fa-plus-circle');
+var Minus = document.getElementsByClassName('fa-minus-circle');
+var quantities = document.getElementsByClassName('quantity');
+var favs =document.getElementsByClassName('fa-heart');
+console.log(favs)
+
+
+for (let i = 0; i < favs.length; i++) {
+    favs[i].addEventListener('click', function() {
+        if (favs[i].style.color=="black") {
+            favs[i].style.color="red";
+        }})
     }
-    else{
-        return cpt
+  
+for (let i = 0; i < Plus.length; i++) {
+    Plus[i].addEventListener('click', function() {
+
+        let quantity = quantities[i];
+        let cpt = parseInt(quantity.innerText);
+
+
+        cpt = cpt + 1;
+        quantity.innerText = cpt;
+
+
+        updateTotalPrice();
+    });
+
+    Minus[i].addEventListener('click', function() {
+
+        let quantity = quantities[i];
+        let cpt = parseInt(quantity.innerText);
+
+
+        if (cpt > 0) {
+            cpt = cpt - 1;
+            quantity.innerText = cpt;
+        }
+
+
+        updateTotalPrice();
+    });
+}
+
+function updateTotalPrice() {
+    var total = 0;
+    var unitPrices = document.getElementsByClassName('unit-price');
+
+    for (let i = 0; i < quantities.length; i++) {
+        let quantity = parseInt(quantities[i].innerText);
+        let unitPrice = parseInt(unitPrices[i].innerText.replace(' $', ''));
+        total += quantity * unitPrice;
     }
-});
+
+    document.querySelector('.total').innerText = total + ' $';
+}
+
